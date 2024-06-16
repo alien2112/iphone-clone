f9610d68f3ee5134014ad41f39d94cc01f98a058
alien2112 <eslamabdatif@gmail.com>
2024-06-17 01:24:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Model from "./components/Model"  
+import Features from "./components/Features"
+
+import * as Sentry from '@sentry/react';
+
+function App() {
+
+
+  return (
+    <main className="bg-black">
+      <Navbar/> 
+      <Hero/>
+      <Highlights/>
+      {/* <Model/> */}
+      <Features/>
+    </main>
+  )
+}
+
+export default Sentry.withProfiler(App);
